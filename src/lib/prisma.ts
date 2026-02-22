586acda849b83269bd46ef619c96ac091a080957
vadaojunior <junior.vads@gmail.com>
2026-02-21 22:06:31 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
     return new PrismaClient({
-        datasource: {
-            url: process.env.DATABASE_URL
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL
+            }
         }
     })
 }
+
 
 
 declare global {
